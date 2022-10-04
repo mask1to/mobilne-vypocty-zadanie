f4213ff1b9c2b25d5c5e32422c82267a2c99c398
Samuel Adler <masko552@gmail.com>
2022-10-04 23:17:34 +0200

    fragmentUpdate
    Updated fragments:
    - intro_fragment
    - form_fragment
    - fetch_fragment

--- app/src/main/java/com/example/semestralnezadanie/fragments/FetchFragment.kt
+++ app/src/main/java/com/example/semestralnezadanie/fragments/FetchFragment.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.airbnb.lottie.LottieAnimationView
 import com.example.semestralnezadanie.R
 
 class FetchFragment: Fragment(), View.OnClickListener,
     ActivityCompat.OnRequestPermissionsResultCallback
 {
+
+    private lateinit var txtName : TextView
+    private lateinit var txtCompany : TextView
+    private lateinit var animationView: LottieAnimationView
+    private lateinit var showOnMapBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -21,6 +30,11 @@ class FetchFragment: Fragment(), View.OnClickListener,
                               savedInstanceState: Bundle?): View?
     {
         return inflater.inflate(R.layout.fetch_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onClick(p0: View?)
--- app/src/main/java/com/example/semestralnezadanie/fragments/FormFragment.kt
+++ app/src/main/java/com/example/semestralnezadanie/fragments/FormFragment.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.semestralnezadanie.R
 
 class FormFragment : Fragment(), View.OnClickListener,
     ActivityCompat.OnRequestPermissionsResultCallback
 {
+
+    private lateinit var nameInput : EditText
+    private lateinit var companyName : EditText
+    private lateinit var companyLatitude : EditText
+    private lateinit var companyLongitude : EditText
+    private lateinit var confirmationBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -21,6 +31,26 @@ class FormFragment : Fragment(), View.OnClickListener,
                               savedInstanceState: Bundle?): View?
     {
         return inflater.inflate(R.layout.form_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        super.onViewCreated(view, savedInstanceState)
+
+        nameInput = view.findViewById(R.id.nameTxt)
+        companyName = view.findViewById(R.id.companyName)
+        companyLatitude = view.findViewById(R.id.companyLatitude)
+        companyLongitude = view.findViewById(R.id.companyLongitude)
+        //val nameInputTxt = nameInput.text.toString()
+        //val companyNameTxt = companyName.text.toString()
+        //val companyLatitudeFloat = companyLatitude.text.toString().toFloat()
+        //val companyLongitudeFloat = companyLongitude.text.toString().toFloat()
+
+        view.findViewById<View>(R.id.confirmationBtn).setOnClickListener {
+            //val action = FormFragmentDirections.actionFormFragmentToFetchFragment(nameInputTxt, companyNameTxt, companyLatitudeFloat, companyLatitudeFloat)
+            val action = FormFragmentDirections.actionFormFragmentToFetchFragment()
+            view.findNavController().navigate(action)
+        }
     }
 
     override fun onClick(p0: View?) {
